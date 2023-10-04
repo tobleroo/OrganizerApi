9277dcc3e3dcc820fc004822ced730b874342136
Tobias J <tobias.jaks@gmail.com>
2023-10-04 09:55:31 +0200

    shoppinglist algo and endpoint implemented and working on first try

--- Cookbook/CookbookControllers/MealPlanerController.cs
+++ Cookbook/CookbookControllers/MealPlanerController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OrganizerApi.Cookbook.CookModels;
+using OrganizerApi.Cookbook.CookModels.CookbookDTOs;
 using OrganizerApi.Cookbook.CookRepository;
 using OrganizerApi.Cookbook.CookServices;
 using OrganizerBlazor.Models;
@@ -43,6 +44,20 @@ namespace OrganizerApi.Cookbook.CookCrudControllers
                 // Handle the exception and return an appropriate response
                 return BadRequest("An error occurred: " + ex.Message);
             }
+        }
+
+        [HttpPost("shoppinglist")]
+        public async Task<ActionResult<List<ShoppingListRecipeDetails>>> CreateShoppingList([FromBody] List<ShoppingListDetailsDTO> recipiesToUse)
+        {
+            var name = User.FindFirstValue(ClaimTypes.Name);
+            var cookBook = await _cookbookRepository.GetCookBook(name);
+            if (cookBook == null)
+            {
+                return NotFound();
+            }
+
+            var finishedShoppingList = ShoppingListCreator.CreateShoppingList(recipiesToUse, cookBook.Recipes);
+            return Ok(finishedShoppingList);
         }
     }
 }
