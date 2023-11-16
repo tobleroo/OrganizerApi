b25015be0dae0aa36ac8f67921a5f8e0fac5b272
Tobias J <tobias.jaks@gmail.com>
2023-11-16 18:41:12 +0100

    re-modeeled whole proejct for test project and sln

---
+++ OrganizerApiTests/Cookbook/CookBookUtils/MealPlannerTests.cs
@@ -0,0 +1,236 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrganizerApi.Cookbook.CookBookUtils;
+using OrganizerApi.Cookbook.CookModels;
+using OrganizerBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerApi.Cookbook.CookBookUtils.Tests
+{
+    [TestClass()]
+    public class MealPlannerTests
+    {
+        [TestMethod()]
+        public void CreateEasyMealPlan_Working()
+        {
+            // :)
+
+            List<RecipeRequestEasyDTO> requestData = new()
+            {
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Easy",
+                    Category = "Breakfast",
+                    MaxCookTime = 100
+                },
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Medium",
+                    Category = "Lunch",
+                    MaxCookTime = 100
+                },
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Hard",
+                    Category = "Dinner",
+                    MaxCookTime = 100
+                }
+            };
+
+            List<Recipe> existingRecipies = new()
+            {
+                new Recipe(){
+                    RecipeName = "recipe1",
+                    Difficulty = RecipeDifficulties.Easy,
+                    RecipeType = RecipeTypes.Breakfast,
+                    CookTime = 1
+                },
+                new Recipe(){
+                    RecipeName = "recipe2",
+                    Difficulty = RecipeDifficulties.Medium,
+                    RecipeType = RecipeTypes.Lunch,
+                    CookTime = 1
+                },
+                new Recipe(){
+                    RecipeName = "recipe3",
+                    Difficulty = RecipeDifficulties.Hard,
+                    RecipeType = RecipeTypes.Dinner,
+                    CookTime = 1
+                }
+            };
+            
+
+            var result = MealPlanner.CreateEasyMealPlan(requestData, existingRecipies);
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual("recipe1", result[0].RecipeName);
+            CollectionAssert.AreEqual(existingRecipies, result);
+        }
+
+        [TestMethod]
+        public void CreateEasyMealPlan_WithNullValues()
+        {
+            var result = MealPlanner.CreateEasyMealPlan(null, null);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void CreateEasyMealPlan_WithEmptyLists()
+        {
+            List<RecipeRequestEasyDTO> requestData = new();
+            List<Recipe> existingRecipies = new();
+
+            var result = MealPlanner.CreateEasyMealPlan(requestData, existingRecipies);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CreateEasyMealPlan_TestEachDifficultyLevel()
+        {
+            List<RecipeRequestEasyDTO> requestData = new()
+            {
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Easy",
+                    Category = "Breakfast",
+                    MaxCookTime = 100
+                },
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Medium",
+                    Category = "Lunch",
+                    MaxCookTime = 100
+                },
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Hard",
+                    Category = "Dinner",
+                    MaxCookTime = 100
+                }
+            };
+
+            List<Recipe> existingRecipies = new()
+            {
+                new Recipe(){
+                    RecipeName = "recipe1",
+                    Difficulty = RecipeDifficulties.Easy,
+                    RecipeType = RecipeTypes.Breakfast,
+                    CookTime = 1
+                },
+                new Recipe(){
+                    RecipeName = "recipe2",
+                    Difficulty = RecipeDifficulties.Medium,
+                    RecipeType = RecipeTypes.Lunch,
+                    CookTime = 1
+                },
+                new Recipe(){
+                    RecipeName = "recipe3",
+                    Difficulty = RecipeDifficulties.Hard,
+                    RecipeType = RecipeTypes.Dinner,
+                    CookTime = 1
+                }
+            };
+
+            var result = MealPlanner.CreateEasyMealPlan(requestData, existingRecipies);
+
+            Assert.IsTrue(result.Any(r => r.Difficulty == RecipeDifficulties.Easy));
+            Assert.IsTrue(result.Any(r => r.Difficulty == RecipeDifficulties.Medium));
+            Assert.IsTrue(result.Any(r => r.Difficulty == RecipeDifficulties.Hard));
+        }
+
+        [TestMethod]
+        public void CreateEasyMealPlan_TestEachRecipeType()
+        {
+            List<RecipeRequestEasyDTO> requestData = new()
+            {
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Easy",
+                    Category = "Breakfast",
+                    MaxCookTime = 100
+                },
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Medium",
+                    Category = "Lunch",
+                    MaxCookTime = 100
+                },
+                new RecipeRequestEasyDTO(){
+                    Difficulty = "Hard",
+                    Category = "Dinner",
+                    MaxCookTime = 100
+                }
+            };
+
+            List<Recipe> existingRecipies = new()
+            {
+                new Recipe(){
+                    RecipeName = "recipe1",
+                    Difficulty = RecipeDifficulties.Easy,
+                    RecipeType = RecipeTypes.Breakfast,
+                    CookTime = 1
+                },
+                new Recipe(){
+                    RecipeName = "recipe2",
+                    Difficulty = RecipeDifficulties.Medium,
+                    RecipeType = RecipeTypes.Lunch,
+                    CookTime = 1
+                },
+                new Recipe(){
+                    RecipeName = "recipe3",
+                    Difficulty = RecipeDifficulties.Hard,
+                    RecipeType = RecipeTypes.Dinner,
+                    CookTime = 1
+                }
+            };
+
+            var result = MealPlanner.CreateEasyMealPlan(requestData, existingRecipies);
+
+            Assert.IsTrue(result.Any(r => r.RecipeType == RecipeTypes.Breakfast));
+            Assert.IsTrue(result.Any(r => r.RecipeType == RecipeTypes.Lunch));
+            Assert.IsTrue(result.Any(r => r.RecipeType == RecipeTypes.Dinner));
+        }
+
+        // next method in class
+
+        [TestMethod]
+        public void CreateRecipeListForSpecificGenerator_Working()
+        {
+
+            var mockCookBook = new UserCookBook()
+            {
+                OwnerUsername = "admin",
+                Recipes = new List<Recipe>
+                {
+                    new Recipe { RecipeName = "Pancakes", RecipeType = RecipeTypes.Breakfast, Guid = Guid.NewGuid() },
+                    new Recipe { RecipeName = "Sandwich", RecipeType = RecipeTypes.Lunch, Guid = Guid.NewGuid() },
+                    new Recipe { RecipeName = "Steak", RecipeType = RecipeTypes.Dinner, Guid = Guid.NewGuid() },
+                    new Recipe { RecipeName = "Cake", RecipeType = RecipeTypes.Dessert, Guid = Guid.NewGuid() },
+                    new Recipe { RecipeName = "Chips", RecipeType = RecipeTypes.Snack, Guid = Guid.NewGuid() }
+                }
+            };
+
+            // Act
+            var result = MealPlanner.CreateRecipeListForSepcificGenerator(mockCookBook);
+
+            // Assert
+            Assert.AreEqual(1, result["Breakfast"].Count);
+            Assert.AreEqual("Pancakes", result["Breakfast"].First().RecipeName);
+        }
+
+        [TestMethod]
+        public void CreateRecipeListForSepcificGenerator_WithEmptyCookbook_ReturnsEmptyCategories()
+        {
+            // Arrange
+            var emptyCookbook = new UserCookBook() {
+                OwnerUsername = "admin",
+                Recipes = new List<Recipe>()
+            };
+
+            // Act
+            var result = MealPlanner.CreateRecipeListForSepcificGenerator(emptyCookbook);
+
+            // Assert
+            Assert.AreEqual(0, result["Breakfast"].Count);
+        }
+    }
+}
---
+++ OrganizerApiTests/Cookbook/CookBookUtils/ShoppingListCreatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrganizerApi.Cookbook.CookBookUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerApi.Cookbook.CookBookUtils.Tests
+{
+    [TestClass()]
+    public class ShoppingListCreatorTests
+    {
+        [TestMethod()]
+        public void AddDateToAdditionalItemLatestUseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckIfItIsTimeToBuyAgainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
