6d33556579de39170ddcb7a0d17ed226514c5d16
Tobias J <tobias.jaks@gmail.com>
2023-11-21 12:37:29 +0100

    testing pull req

--- OrganizerApi/WeatherForecast.cs
+++ OrganizerApi/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace OrganizerApi
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        // testing pull request 
     }
 }
